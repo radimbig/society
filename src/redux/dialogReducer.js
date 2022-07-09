b01070bac726c83e95bc1de50c31fb41abc66384
radimbig <radimvadim@gmail.com>
2022-07-09 16:25:59 +0200

    redusers refactoring

--- src/redux/dialogReducer.js
+++ src/redux/dialogReducer.js
@@ -1,56 +1,56 @@
+const TEMP_MESS = "TEMP-MESS";
+const SEND_MESS = "SEND-MESS";
 
-const TEMP_MESS = "TEMP-MESS"
-const SEND_MESS = "SEND-MESS"
+let initializationState = {
+  chatsData: [
+    { id: "1", name: "radim", profileLink: "" },
+    {
+      id: "2",
+      name: "vadim",
+      profileLink:
+        "https://iconarchive.com/download/i102645/graphicloads/flat-finance/person.ico",
+    },
+    {
+      id: "3",
+      name: "eva",
+      profileLink:
+        "https://icon-library.com/images/steam-icon-ico/steam-icon-ico-16.jpg",
+    },
+    {
+      id: "4",
+      name: "masha",
+      profileLink:
+        "https://iconarchive.com/download/i48697/custom-icon-design/pretty-office-2/man.ico",
+    },
+  ],
+  messeages: [
+    { id: 1, text: "Hello my name Radim" },
+    { id: 2, text: "What`s your name?" },
+    { id: 3, text: "my name`s Radim!" },
+  ],
+  temp: "My message",
+};
 
-let initializationState ={
-    chatsData: [
-      { id: "1", name: "radim", profileLink: "" },
-      {
-        id: "2",
-        name: "vadim",
-        profileLink:
-          "https://iconarchive.com/download/i102645/graphicloads/flat-finance/person.ico",
-      },
-      {
-        id: "3",
-        name: "eva",
-        profileLink:
-          "https://icon-library.com/images/steam-icon-ico/steam-icon-ico-16.jpg",
-      },
-      {
-        id: "4",
-        name: "masha",
-        profileLink:
-          "https://iconarchive.com/download/i48697/custom-icon-design/pretty-office-2/man.ico",
-      },
-    ],
-    messeages:[
-      {id:1, text:"Hello my name Radim"},
-      {id:2, text:"What`s your name?"},
-      {id:3, text:"my name`s Radim!"}
-    ],
-    temp:"My message"
+const dialogsReduser = (state = initializationState, action) => {
+  switch (action.type) {
+    case TEMP_MESS:
+      return {
+        ...state,
+        temp: action.text,
+      };
+    case SEND_MESS:
+      let newMess = {
+        id: state.messeages.length + 1,
+        text: state.temp,
+      };
+      return {
+        ...state,
+        messeages: [...state.messeages, newMess],
+        temp: "",
+      };
+    default:
+      return state;
   }
+};
 
- const dialogsReduser = (state = initializationState, action) =>{
-  let tempState = {...state}
-  tempState.messeages = [...state.messeages]
-    switch(action.type){
-        case TEMP_MESS:
-          
-            tempState.temp = action.text;
-            return tempState;
-        case SEND_MESS:
-            let newMess = {
-                id:tempState.messeages.length+1,
-                text:tempState.temp
-              }
-              tempState.messeages.push(newMess)
-              tempState.temp = ""
-              return tempState
-        default: return state      
-    }
-
-}
-
-export default dialogsReduser
+export default dialogsReduser;
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,39 +1,36 @@
 const TEMP_POST = "TEMP-POST";
 const ADD_POST = "ADD-POST";
 
-
 let initializationState = {
-    postsObj: [
-      { id: 1, likes: 133, text: "ja churka" },
-      { id: 2, likes: 45, text: "lysa boshka" },
-      { id: 3, likes: 89, text: "kruchu binance" },
-      { id: 4, likes: 22, text: "bebromentr" },
-    ],
-    tempPost: "Radim",
+  postsObj: [
+    { id: 1, likes: 133, text: "ja churka" },
+    { id: 2, likes: 45, text: "lysa boshka" },
+    { id: 3, likes: 89, text: "kruchu binance" },
+    { id: 4, likes: 22, text: "bebromentr" },
+  ],
+  tempPost: "Radim",
+};
+
+const profileReducer = (state = initializationState, action) => {
+  switch (action.type) {
+    case ADD_POST:
+      let NewPost = {
+        id: state.postsObj.length + 1,
+        likes: 0,
+        text: state.tempPost,
+      };
+      return {
+        ...state,
+        tempPost: "",
+        postsObj: [...state.postsObj, NewPost],
+      };
+    case TEMP_POST:
+      return {
+        ...state,
+        tempPost: action.text,
+      };
+    default:
+      return state;
   }
-
-const profileReducer = (state = initializationState, action) =>{
-  let tempState = {...state}
-  tempState.postsObj = [...state.postsObj]
-    switch(action.type){
-        case ADD_POST:
-            let NewPost = {
-                id: tempState.postsObj.length + 1,
-                likes: 0,
-                text: tempState.tempPost,
-              };
-              tempState.postsObj.push(NewPost);
-              tempState.tempPost = "";
-              return tempState
-        case TEMP_POST:
-            tempState.tempPost = action.text;
-            return tempState
-        default: 
-              return state
-
-
-    }
-
-
-}
-export default profileReducer
+};
+export default profileReducer;
